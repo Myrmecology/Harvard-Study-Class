d6241ee8d17bbf19cbb08d1d719d60b741392c6d
Justin <185717017+Myrmecology@users.noreply.github.com>
2026-01-19 18:24:41 -0700

    study starter

---
+++ README.md
@@ -0,0 +1,2 @@
+## This is a study project for Harvard University 
+this will be an ongoing repo until the end of the semester
---
+++ hello.py
@@ -0,0 +1,6 @@
+# ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+print("hello,")
+print(name)
